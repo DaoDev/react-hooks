b0beb86d01cf9200559e26919435df0b294aa5e7
DaoDev <boynguyen1998@gmail.com>
2021-05-10 14:42:22 +0700

    final react hooks basic

--- src/App.js
+++ src/App.js
@@ -1,61 +1,25 @@
 import { useEffect, useState } from "react";
 import "./App.scss";
+import BetterClock from "./components/BetterClock";
+import Clock from "./components/Clock";
 import ColorBox from "./components/ColorBox";
+import Hero from "./components/Hero";
+import MagicBox from "./components/MagicBox";
 import PostList from "./components/PostList";
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
 
 function App() {
-  const [todoList, setTodoList] = useState([
-    { id: 1, title: "Hello" },
-    { id: 2, title: "Hi" },
-    { id: 3, title: "Bye bye" },
-  ]);
-
-  const [postList, setPostList] = useState([]);
-
-  useEffect(() => {
-    const fetchPostList = async () => {
-      try {
-        const requestUrl =
-          "http://js-post-api.herokuapp.com/api/posts?_limit=10&_page=1";
-        const response = await fetch(requestUrl);
-        const responseJSON = await response.json();
-        console.log({ responseJSON, response });
-        const { data } = responseJSON;
-        setPostList(data);
-      } catch (error) {
-        console.log("Failed to fetch post list: ", error.message);
-      }
-    };
-    fetchPostList();
-  }, []);
-
-  const handleTodoClick = (todo) => {
-    const index = todoList.findIndex((x) => x.id === todo.id);
-    if (index < 0) return;
-    const newTodoList = [...todoList];
-    newTodoList.splice(index, 1);
-    setTodoList(newTodoList);
-  };
-
-  const handleTodoFormSubmit = (formValue) => {
-    const newTodo = {
-      id: todoList.length + 1,
-      ...formValue,
-    };
-    const newTodoList = [...todoList];
-    newTodoList.push(newTodo);
-    setTodoList(newTodoList);
-  };
+  const [count, setCount] = useState(0);
 
   return (
     <div className="app">
       <h1>Welcome to React Hooks</h1>
-      <PostList posts={postList} />
-      {/* <ColorBox /> */}
-      {/* <TodoForm onSubmit={handleTodoFormSubmit} /> */}
-      {/* <TodoList todos={todoList} onToDoClick={handleTodoClick} /> */}
+      <p>{count}</p>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+      <button onClick={() => setCount(count - 1)}>Reduction</button>
+
+      <Hero name={"Ez FE"} />
     </div>
   );
 }
